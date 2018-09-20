57e9f2dd6a5e93abb415984097753b1c6f89ee90
Tony Bowden <tony@tmtm.com>
2018-09-20 14:05:54 +0100

    Update for changed layout

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,9 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://en.wikipedia.org/wiki/List_of_MPs_of_the_National_Assembly_of_Cambodia',
-  xpath: '//table[.//th[contains(.,"MP")]]//tr[td]//td[2]//a[not(@class="new")]/@title',
-) 
+names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_National_Assembly_of_Cambodia,_2013%E2%80%9318',
+  xpath: '//table[.//th[contains(.,"Constituency")]]//tr[td]//td[last()]//a[not(@class="new")]/@title'
+)
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
 
